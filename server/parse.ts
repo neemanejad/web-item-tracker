b2732a8002a11c2b5f8904434dc51b66cf70c036
Neema Nejad <dnnejad@gmail.com>
2023-04-01 18:02:23 -0700

    created unsafe way for getting text from xpath using puppeteer

--- server/parse.ts
+++ server/parse.ts
@@ -1,8 +1,51 @@
+import puppeteer, { PuppeteerLaunchOptions } from 'puppeteer';
 import { WebItemRequest, WebItemResponse } from './types';
 
 export async function parseWebItems(
   url: string,
   items: WebItemRequest[],
 ): Promise<WebItemResponse[]> {
-  return items.map(({ name, xpath }) => ({ name, value: `${xpath}'s value` }));
+  // unsafe with --no-sandbox argument
+  const browser = await puppeteer.launch({
+    executablePath: process.env['CHROMIUM_BIN'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  const promises = items.map((item) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        console.log(`[server]: waiting for xpath: ${item.name}`);
+        await page.waitForXPath(item.xpath, { timeout: 60000});
+
+        console.log(`[server]: getting element with xpath '${item.xpath}'`)
+        const elHandle = await page.$x(item.xpath);
+
+        console.log(`[server]: evaluating xpath '${item.xpath}'`)
+        const value = await page.evaluate((el) => el.textContent, elHandle[0]);
+        console.log(`[server]: retrieved value for ${item.name} is '${value}'`);
+        resolve(value);
+      } catch (err) {
+        console.log(
+          `[server]: error parsing xpath '${item.xpath} for ${item.name}: ${err}`,
+        );
+        reject(err);
+      }
+    })
+      .then(
+        (value): WebItemResponse => ({
+          name: item.name,
+          value: value as string,
+        }),
+      )
+      .catch((err): WebItemResponse => ({ name: item.name, value: 'ERROR' }));
+  });
+
+  const responses = await Promise.all(promises);
+
+  await browser.close();
+
+  return responses;
 }
